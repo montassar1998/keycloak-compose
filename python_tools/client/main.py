e67c29c8f870bce4fbd2529133a65c136d280881
montassar1998 <montassarriahi@aol.com>
2023-08-10 12:49:02 +0000

    fd

--- python_tools/client/main.py
+++ python_tools/client/main.py
@@ -8,16 +8,17 @@ CLIENT_NAME = "pyclient"
 USERNAME = os.getenv("USERNAME")
 USER_PASSWORD = os.getenv("USER_PASSWORD")
 
-keycloak_openid = KeycloakOpenID(
-    server_url=f"{KEYCLOAK_URL}:{KEYCLOAK_PORT}/auth/",
-    client_id=CLIENT_NAME,
-    realm_name=REALM_NAME
-)
+while True:
+    keycloak_openid = KeycloakOpenID(
+        server_url=f"{KEYCLOAK_URL}:{KEYCLOAK_PORT}/auth/",
+        client_id=CLIENT_NAME,
+        realm_name=REALM_NAME
+    )
 
-# Obtain a token using the "password" grant type
-token_response = keycloak_openid.token(USERNAME, USER_PASSWORD)
-print(token_response)
+    # Obtain a token using the "password" grant type
+    token_response = keycloak_openid.token(USERNAME, USER_PASSWORD)
+    print(token_response)
 
-# You can extract the actual access token from the response
-access_token = token_response.get('access_token')
-print(access_token)
+    # You can extract the actual access token from the response
+    access_token = token_response.get('access_token')
+    print(access_token)
--- python_tools/importer/importer.py
+++ python_tools/importer/importer.py
@@ -8,29 +8,30 @@ REALM_NAME = os.getenv("KC_REALM_NAME")
 IMPORTER_NAME = os.getenv("IMPORTER_NAME")
 IMPORTER_PASSWORD = os.getenv("IMPORTER_PASSWORD")
 
-# Create a Keycloak admin connection
-keycloak_admin = KeycloakAdmin(
-    server_url=f"{KEYCLOAK_URL}:{KEYCLOAK_PORT}/auth/",
-    username=IMPORTER_NAME,
-    password=IMPORTER_PASSWORD,
-    realm_name=REALM_NAME,
-    client_id="pyclient",
-    verify=True # Change this to the path to your certificate if using self-signed, or False to disable verification.
-)
 
-# Fetch users from the /valid_users endpoint
-response = requests.get("http://client_generator:5000/valid_users")
-users = response.json()
+while true:
+    # Create a Keycloak admin connection
+    keycloak_admin = KeycloakAdmin(
+        server_url=f"{KEYCLOAK_URL}:{KEYCLOAK_PORT}/auth/",
+        username=IMPORTER_NAME,
+        password=IMPORTER_PASSWORD,
+        realm_name=REALM_NAME,
+        client_id="pyclient",
+        verify=True # Change this to the path to your certificate if using self-signed, or False to disable verification.
+    )
+    # Fetch users from the /valid_users endpoint
+    response = requests.get("http://client_generator:5000/valid_users")
+    users = response.json()
 
-# Convert the users to the format expected by Keycloak and import them
-for user in users:
-    user_representation = {
-        "username": user["username"],
-        "enabled": True,
-        "credentials": [{"type": "password", "value": user["password"], "temporary": False}]
-    }
-    try:
-        keycloak_admin.create_user(user_representation)
-        print(f"Successfully created user {user['username']}")
-    except Exception as e:
-        print(f"Failed to create user {user['username']}: {str(e)}")
+    # Convert the users to the format expected by Keycloak and import them
+    for user in users:
+        user_representation = {
+            "username": user["username"],
+            "enabled": True,
+            "credentials": [{"type": "password", "value": user["password"], "temporary": False}]
+        }
+        try:
+            keycloak_admin.create_user(user_representation)
+            print(f"Successfully created user {user['username']}")
+        except Exception as e:
+            print(f"Failed to create user {user['username']}: {str(e)}")
