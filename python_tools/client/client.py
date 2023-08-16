861a5668497f733a60029746652504b520d31c22
montassar1998 <montassarriahi@aol.com>
2023-08-16 08:15:12 +0000

    fds

--- python_tools/client/client.py
+++ python_tools/client/client.py
@@ -1,33 +1,53 @@
-import os
 import requests
 from flask import Flask, jsonify
+import os 
 import time
 
-# Configuration
-KEYCLOAK_URL = os.getenv("KC_HOSTNAME", "http://keycloak:8080")
-REALM_NAME = os.getenv("KC_REALM_NAME")
-CLIENT_NAME = "pyclient"
-USERNAME = os.getenv("USERNAME")
-USER_PASSWORD = os.getenv("USER_PASSWORD")
-ALL_USERS_URL = os.getenv("ALL_USERS_URL", "http://client_generator:5000/all_users")
-TOKEN_ENDPOINT_URL = f"{KEYCLOAK_URL}/auth/realms/{REALM_NAME}/protocol/openid-connect/token"
+GENERATOR_NAME = os.getenv("GENERATOR_NAME")
+# Define Keycloak parameters
+KEYCLOAK_URL = "http://keycloak:8080"
+REALM = "master"
+CLIENT_ID = "pyclient"
+USERNAME = "admin"
+PASSWORD = "keycloak"
+
+# URL for the token endpoint
+token_url = f"{KEYCLOAK_URL}/realms/{REALM}/protocol/openid-connect/token"
+SERVICE_URL = os.getenv("VALID_USERS")
+ADMIN_API_URL = f"{KEYCLOAK_URL}/admin/realms/{REALM}/users"
+ADMIN_ACCESS_TOKEN_URL = f"{KEYCLOAK_URL}/realms/{REALM}/protocol/openid-connect/token"
+
+
+app = Flask(__name__)
+def get_admin_access_token():
+    data = {
+        "grant_type": "password",
+        "client_id": CLIENT_ID,
+        "username": USERNAME,
+        "password": PASSWORD
+    }
+    response = requests.post(ADMIN_ACCESS_TOKEN_URL, data=data)
+    if response.status_code == 200:
+        return response.json()["access_token"]
+    else:
+        return None
 
 MAX_RETRIES = 100
 RETRY_INTERVAL = 10  # seconds
-app = Flask(__name__)
 
 def is_keycloak_up():
     try:
-        response = requests.get(f"{KEYCLOAK_URL}/auth")
+        response = requests.get(KEYCLOAK_URL)
+        print(f"Caught Keycloak Up ************************************\n"*100)
         response.raise_for_status()
         return True
     except requests.RequestException:
         return False
 
-# Check if Keycloak is up
 retries = 0
 while retries < MAX_RETRIES:
     if is_keycloak_up():
+        # Continue with the rest of your application logic
         break
     time.sleep(RETRY_INTERVAL)
     retries += 1
