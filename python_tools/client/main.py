a8ee1fa8003c3cf4da90589f9a54b9a5c9267af6
montassar1998 <montassarriahi@aol.com>
2023-08-16 08:10:15 +0000

    fds

--- python_tools/client/main.py
+++ python_tools/client/main.py
@@ -1,49 +1,33 @@
 import os
-from keycloak import KeycloakOpenID
 import requests
-import logging
 from flask import Flask, jsonify
 import time
-KEYCLOAK_URL = os.getenv("KC_HOSTNAME","http://keycloak:8080")
-KEYCLOAK_PORT = os.getenv("KEYCLOAK_PORT")
+
+# Configuration
+KEYCLOAK_URL = os.getenv("KC_HOSTNAME", "http://keycloak:8080")
 REALM_NAME = os.getenv("KC_REALM_NAME")
 CLIENT_NAME = "pyclient"
 USERNAME = os.getenv("USERNAME")
 USER_PASSWORD = os.getenv("USER_PASSWORD")
 ALL_USERS_URL = os.getenv("ALL_USERS_URL", "http://client_generator:5000/all_users")
+TOKEN_ENDPOINT_URL = f"{KEYCLOAK_URL}/auth/realms/{REALM_NAME}/protocol/openid-connect/token"
+
 MAX_RETRIES = 100
 RETRY_INTERVAL = 10  # seconds
-
-
-TOKEN_ENDPOINT_URL = f"{KEYCLOAK_URL}/realms/{REALM_NAME}/protocol/openid-connect/token"
-print("this is the pyclient ")
 app = Flask(__name__)
-def get_admin_access_token():
-    data = {
-        "grant_type": "password",
-        "client_id": CLIENT_NAME,
-        "username": USERNAME,
-        "password": PASSWORD
-    }
-    response = requests.post(TOKEN_ENDPOINT_URL, data=data)
-    if response.status_code == 200:
-        return response.json()["access_token"]
-    else:
-        return None
 
 def is_keycloak_up():
     try:
-        response = requests.get(KEYCLOAK_URL)
-        print(f"Keycloak response {response}")
+        response = requests.get(f"{KEYCLOAK_URL}/auth")
         response.raise_for_status()
         return True
     except requests.RequestException:
         return False
 
+# Check if Keycloak is up
 retries = 0
 while retries < MAX_RETRIES:
     if is_keycloak_up():
-        # Continue with the rest of your application logic
         break
     time.sleep(RETRY_INTERVAL)
     retries += 1
@@ -51,20 +35,19 @@ else:
     print("Keycloak is still not up after several retries. Exiting...")
     exit(1)
 
-
 def authenticate_user(username, password):
     data = {
         "grant_type": "password",
-        "client_id": CLIENT_ID,
+        "client_id": CLIENT_NAME,
         "username": username,
         "password": password
     }
 
     # Send authentication request
-    response = requests.post(token_url, data=data)
+    response = requests.post(TOKEN_ENDPOINT_URL, data=data)
     
     if response.status_code == 200:
-        print(f"User {username} with password: {password} Authenticated Successfully!")
+        print(f"User {username} Authenticated Successfully!")
         return True
     else:
         print(f"Authentication failed for user {username}: {response.status_code}")
@@ -82,14 +65,12 @@ def authenticate_users():
         for user in all_users:
             if authenticate_user(user['username'], user['password']):
                 authenticated_count += 1
-        exit(0)
+
         return jsonify({"message": f"Authenticated {authenticated_count} out of {len(all_users)} users."})
     except requests.RequestException as e:
         print(f"Error fetching all users: {e}")
         return jsonify({"message": "Failed to fetch all users", "error": str(e)}), 500
-        exit(1)
 
 if __name__ == "__main__":
-    with app.app_context(): 
-        authenticate_users()
+    authenticate_users()
     app.run(host='0.0.0.0', debug=True, port=5002)
