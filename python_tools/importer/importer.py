21f3307e8b1314c40560b166a6ffdb41098d9b8d
montassar1998 <montassarriahi@aol.com>
2023-08-17 10:05:14 +0000

    remove init

--- python_tools/importer/importer.py
+++ python_tools/importer/importer.py
@@ -118,9 +118,6 @@ def create_users():
     print(f"the value of IsImport is {isImportDone}")
     isImportDone=True
     return jsonify({"message": f"Created {users_created} users in Keycloak"})
-def initialize_users():
-    with app.app_context():
-        create_users()
 if __name__ == "__main__":
-    initialize_users()
+
     app.run(host='0.0.0.0', debug=True, port=5001)
