313b1b4eed47fed93471190cecdae84ec4b64db9
montassar1998 <montassarriahi@aol.com>
2023-09-05 10:28:26 +0000

    g

--- python_tools/importer/importer.py
+++ python_tools/importer/importer.py
@@ -168,9 +168,7 @@ def initialize_app():
             create_users()
         initialized = True
 
-@app.route('/metrics')
-def expose_metrics():
-    return metrics.export(), 200
+
     
 def main():
     with app.app_context():
