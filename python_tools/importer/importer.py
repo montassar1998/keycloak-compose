157ecf563be8ac8263a82874243158f4c8bf524c
montassar1998 <montassarriahi@aol.com>
2023-09-06 09:43:39 +0000

    gfd

--- python_tools/importer/importer.py
+++ python_tools/importer/importer.py
@@ -42,10 +42,6 @@ total_requests_metric = metrics.counter('total_requests', 'Total number of reque
 error_rate_metric = metrics.counter('error_rate', 'Error rate of API requests', labels={
                                    'endpoint': lambda: request.endpoint, 'status_code': 'HTTP status code'})
 
-@app.route('/metrics')
-def metrics_exporter():
-    # Your metrics collection logic here
-    return metrics.export(), 200  # Return the metrics in the Prometheus format
     
 def get_admin_access_token():
     data = {
